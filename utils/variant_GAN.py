faa588fc398682982b0fc8167f3103d6ed59b547
fyellow <59092391+fyellow@users.noreply.github.com>
2023-02-21 22:54:41 +0530

    Update variant_GAN.py

--- utils/variant_GAN.py
+++ utils/variant_GAN.py
@@ -2,16 +2,16 @@ import torch
 import torch.nn as nn
 
 
-class Generator(nn.Module):
+class Generator(nn.Module): #noise i/p image o/p
     def __init__(self):
         super(Generator, self).__init__()
         self.latent_dim = 256 #length of generator's latent space
 
-        self.fc_1 = nn.Linear(self.latent_dim, self.latent_dim * 2 * 2) 
+        self.fc_1 = nn.Linear(self.latent_dim, self.latent_dim * 2 * 2) # takes input noise vector-self.latnet_dim
         self.bn_fc = nn.BatchNorm2d(self.latent_dim) 
 
         self.convt_1 = nn.ConvTranspose2d(self.latent_dim, 128, (6, 2), stride=(1, 1)) 
-        self.bn_1 = nn.BatchNorm2d(128)
+        self.bn_1 = nn.BatchNorm2d(128) #improves training process 
 
         self.convt_2 = nn.ConvTranspose2d(128, 32, (6, 2), stride=(2, 1))
         self.bn_2 = nn.BatchNorm2d(32)
@@ -19,11 +19,11 @@ class Generator(nn.Module):
         self.convt_3 = nn.ConvTranspose2d(32, 16, (6, 2), stride=(1, 1))
         self.bn_3 = nn.BatchNorm2d(16)
 
-        self.convt_4 = nn.ConvTranspose2d(16, 8, (3, 2), stride=(2, 1))
+        self.convt_4 = nn.ConvTranspose2d(16, 8, (3, 2), stride=(2, 1))#smaller strides: larger output feature map-higher spartail reso
         self.bn_4 = nn.BatchNorm2d(8)
 
         self.convt_5 = nn.ConvTranspose2d(8, 1, (4, 1), stride=(1, 1))
-        self.LRe = nn.LeakyReLU(negative_slope=0.2)
+        self.LRe = nn.LeakyReLU(negative_slope=0.2) #(0.01 to 0.3)
 
     def forward(self, Input):
         x = self.fc_1(Input).reshape(-1, self.latent_dim, 2, 2)
@@ -51,9 +51,9 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         self.fc_1 = nn.Linear(64 * 2 * 2, 1)
-        self.activation = nn.Sigmoid() #ask audrey why sigmoid--> maybe differntiable and invertable  
+        self.activation = nn.Sigmoid() #binary clasification 
 
-        self.convt_1 = nn.Conv2d(1, 16, (7, 2), stride=(1, 1))
+        self.convt_1 = nn.Conv2d(1, 16, (7, 2), stride=(1, 1)) #filter size-(7,2)
         self.bn_1 = nn.BatchNorm2d(16)
 
         self.convt_2 = nn.Conv2d(16, 32, (6, 2), stride=(2, 1))
