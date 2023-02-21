2358626fd14561c1612c4d7a6cb326f501ee1f2e
fyellow <59092391+fyellow@users.noreply.github.com>
2023-02-21 23:05:55 +0530

    Update losses.py

--- utils/losses.py
+++ utils/losses.py
@@ -11,10 +11,11 @@ class GenFGANLoss(nn.Module):
     def forward(self, d_out, g_out):
         # calculate loss using the function defined in the paper
         bce = torch.nn.BCELoss()
-        EL = bce(d_out, self.alpha * torch.ones_like(d_out))
+        EL = bce(d_out, self.alpha * torch.ones_like(d_out)) #bce-discriminator should output 1 for real samples and 0 for fake samples. 
+        #Helps generator to generate samples to fool discriminator 
         mu = torch.ones_like(g_out) * g_out.mean(dim=0)
-        DL = 1 / torch.mean(torch.norm(g_out - mu, p=2))
-        loss = EL + self.beta * DL
+        DL = 1 / torch.mean(torch.norm(g_out - mu, p=2))#diverse samples
+        loss = EL + self.beta * DL  #when b=0--vanilla gan if b>0-- better samples(diverse)
         return loss
 
 
@@ -26,7 +27,7 @@ class DiscFGANLoss(nn.Module):
     def forward(self, d_out_real, d_out_fake):
         # calculate loss using the function defined in the paper
         loss = torch.mean(-torch.log(d_out_real) - self.gamma * torch.log(torch.ones_like(d_out_fake) - d_out_fake))
-        return loss
+        return loss # relative importance of the two terms in the loss function(gamma)
 
 
 class PretrainLoss(nn.Module): #ask audery 
